977fa190ed5aba14889ed47feadb94a619a2fd7c
Eric Yi <ericyi2001@gmail.com>
2022-12-18 15:56:26 -0500

    Fix bugs and add 404 page
    - Fixed issues causing bcrypt/login manager import issue by editing
    __init__.py
    - Added 404 page
    - Fixed error caused by incorrect calls to url_for()

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_mongoengine import MongoEngine
 from flask_login import (
     LoginManager,
@@ -8,25 +8,35 @@ from flask_bcrypt import Bcrypt
 from datetime import datetime
 import os
 
+db = MongoEngine()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
-def create_app(test_config=None):
+
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+def create_app(test_config="config.py"):
 
     app = Flask(__name__)
 
     # load config
-    app.config.from_pyfile("config.py", silent=False)
+    app.config.from_pyfile(test_config, silent=False)
 
     # load Flask Mongo, Flask LoginManager, and BCrypt
-    db = MongoEngine(app)
-    login_manager = LoginManager(app)
-    bcrypt = Bcrypt(app)
+    from . import db
+    db.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
 
     from flask_app.users.routes import users
     from flask_app.siteview.routes import site
 
     # Register blueprints
-    app.register_blueprint(users)
+    app.register_blueprint(users, url_prefix="/users")
     app.register_blueprint(site)
+
+    app.register_error_handler(404, page_not_found)
 
     login_manager.login_view = "users.login"
 
