aecc4f6c18c5fc3873019b22f5539c9d9d107eac
Eric Yi <ericyi2001@gmail.com>
2022-12-18 18:12:19 -0500

    Add account view function and new templates
    - Add account() view function in users blueprint
    - Add accouunt template
    - Add query.html and user_detail.html templates for later (to be edited)

--- flask_app/users/routes.py
+++ flask_app/users/routes.py
@@ -1,9 +1,15 @@
-from flask import Blueprint, redirect, url_for, render_template, flash
+from flask import Blueprint, redirect, url_for, render_template, flash, request
 from flask_login import current_user, login_required, login_user, logout_user
 
 from .. import bcrypt
-from ..forms import RegistrationForm, LoginForm
+from ..forms import (RegistrationForm, LoginForm,
+                     UpdateUsernameForm, UpdateProfilePicForm,)
 from ..models import User
+
+from werkzeug.utils import secure_filename
+
+import io
+import base64
 
 users = Blueprint("users", __name__)
 
@@ -16,7 +22,8 @@ def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        hashed = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        hashed = bcrypt.generate_password_hash(
+            form.password.data).decode("utf-8")
         user = User(
             username=form.username.data,
             email=form.email.data,
@@ -61,4 +68,36 @@ def logout():
 @users.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    pass
+    propic = get_b64_img(current_user.username)
+
+    update_username_form = UpdateUsernameForm()
+    update_propic_form = UpdateProfilePicForm()
+
+    if update_username_form.validate_on_submit():
+        current_user.modify(username=update_username_form.username.data)
+        current_user.save()
+
+        return redirect(url_for('users.login'))
+
+    if update_propic_form.validate_on_submit():
+        img = update_propic_form.image.data
+        filename = secure_filename(img.filename)
+        content_type = f'images/{filename[-3:]}'
+
+        if current_user.profile_pic.get() is None:
+            current_user.profile_pic.put(img.stream, content_type=content_type)
+        else:
+            current_user.profile_pic.replace(
+                img.stream, content_type=content_type)
+        current_user.save()
+
+        return redirect(url_for('users.account'))
+
+    return render_template("account.html", update_username_form=update_username_form, update_propic_form=update_propic_form, image=propic)
+
+
+def get_b64_img(username):
+    user = User.objects(username=username).first()
+    bytes_im = io.BytesIO(user.profile_pic.read())
+    image = base64.b64encode(bytes_im.getvalue()).decode()
+    return image
